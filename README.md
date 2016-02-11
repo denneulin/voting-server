9385914a9167fa8388a16accd74fb27f9b24a9a4
Anthony Denneulin <adenneulin.idapps@tf1.fr>
2016-02-11 09:47:52 +0100

    version at the end of the tutorial

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# voting-server
-experiment react with redux
+# Voting client
+Server App from awesome [Full-Stack React-Redux-Immutable Tutorial](http://teropa.info/blog/2015/09/10/full-stack-redux-tutorial.html) by Tero Parviainen.
+
+# Purpose
+Experiment React and Redux.
+
+# Next steps
+- add eslint
+- instead of sending the whole state, send only the modified part
+- dev [exercises](http://teropa.info/blog/2015/09/10/full-stack-redux-tutorial.html#exercises)
+
---
+++ src/server.js
@@ -0,0 +1,16 @@
+import Server from 'socket.io';
+
+export default function startServer(store) {
+  const io = new Server().attach(8090);
+
+  // TODO : optimize this (e.g. just sending the relevant subset, sending diffs instead of snapshots...)
+  store.subscribe(
+    () => io.emit('state', store.getState().toJS())
+  );
+
+  io.on('connection', (socket) => {
+    socket.emit('state', store.getState().toJS());
+    socket.on('action', store.dispatch.bind(store));
+  });
+
+}
